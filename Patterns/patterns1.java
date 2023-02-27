a3206d42cd1889bc706e53fc1aa9555b601a1379
akash-banger <akashbanger2@gmail.com>
2023-02-27 23:11:37 +0530

    first commit

--- Patterns/patterns1.java
+++ Patterns/patterns1.java
@@ -85,12 +85,12 @@ public class patterns1{
             System.out.println("Enter how many lines pattern you wanna create");
             int n = sc.nextInt();
 
-            char ch = 'A';
+            char cha = 'A';
 
             for(int i=1; i<=n; i++){
                 for(int j=1; j<=i; j++){
-                    System.out.print(ch);
-                    ch++;
+                    System.out.print(cha);
+                    cha++;
                 }
                 System.out.println();
             }
