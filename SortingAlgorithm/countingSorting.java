2a1e7e142572b2b6ed00c25f9162c0b1c3fbaf95
sakshishinde1749 <sakshishinde1749@gmail.com>
2023-03-09 00:59:12 +0530

    counting sorting added

--- SortingAlgorithm/countingSorting.java
+++ SortingAlgorithm/countingSorting.java
@@ -1,32 +1,35 @@
+package SortingAlgorithm;
+//this method is used for array in which all numbers are positive and range of array is minimum
+
 public class countingSorting{
 
     public static void countingSort(int numbers[]){
         int max = Integer.MIN_VALUE;
 
+        //finding max value to form new freq array
         for(int i=0; i<numbers.length; i++){
             max = Math.max(max, numbers[i]);
         }
 
         int freq[] = new int[max+1];
 
-        for(int i=0; i<freq.length; i++){
-            int count = 0;
+        //transfering numbers of numbers[] into freq[] in the freq form
+       
 
-            for(int j=0; j<numbers.length; j++){
-                if(numbers[j]==i){
-                    freq[i]= count++;
-                }
-            }
-
+        for(int i=0; i<numbers.length; i++){
+            freq[numbers[i]]++;
         }
-        for(int i=0; i<freq.length; i++){
 
-            for(int j=0; j<numbers.length; j++){
-                if(freq[i]>0){
-                    numbers[j] = i;
-                    freq[i]--;
-                }
+        //sorting
+        int k=0;
+        for(int i=0; i<freq.length; i++){
+            
+            while(freq[i]>0){
+                numbers[k]=i;
+                freq[i]--;
+                k++;
             }
+
         }
 
     }
