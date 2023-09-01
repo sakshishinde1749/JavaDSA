b70ab100c1e6c97cb5dddab60ca019e0165c81b4
Sakshi Shinde <sakshishinde1749@gmail.com>
2023-09-01 16:20:35 +0530

    interface added

---
+++ OOPS/Interfaces.java
@@ -0,0 +1,8 @@
+package OOPS;
+
+// Interfaces : Just like class is a blueprint of object, Interfaces is a blueprint of class 
+// eg. we can make a car blueprint(Interface) then we can keep maruthi800 as class which will be blueprint for object
+
+public class Interfaces {
+    
+}
